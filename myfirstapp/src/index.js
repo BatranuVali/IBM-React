807340fd754cf00609be9cd167ceb06fa15c2bc3
BatranuVali <valentiemsphotografy@gmail.com>
2023-08-16 14:15:25 +0300

    Update current time every second

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
-reportWebVitals();
+function ref() {
+  root.render(<App/>);
+}
+
+setInterval(ref, 1000);
